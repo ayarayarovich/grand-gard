bdf075f18c80734dc878dd81c8d9198a25605e57
Айяр Халилов <super.ayar-04@yandex.ru>
2023-07-12 20:04:38 +0300

    Add database, alembic etc.

--- server/settings.py
+++ server/settings.py
@@ -4,5 +4,5 @@ env = Env()
 
 DATABASE_URL = env.str(
     "DATABASE_URL",
-    default="postgresql+asyncpg://postgres:postgres@localhost:5432/postgres",
+    default="postgresql+asyncpg://gen_user:d|yUIbDU153MG{@grand-db.ayarayarovich.tech:5432/default_db",
 )
