ac648bad97892f0df750dcabc34eb6bfd863e1f3
Evgeniya Rib <--->
2023-07-13 20:56:12 +0300

    Changed main

--- server/main.py
+++ server/main.py
@@ -8,5 +8,6 @@ app.include_router(clients_router, prefix="/clients", tags=["clients"])
 app.include_router(employees_router, prefix="/employees", tags=["employees"])
 app.include_router(rooms_router, prefix="/rooms", tags=["rooms"])
 app.include_router(services_router, prefix="/services", tags=["services"])
-app.include_router(posts_router, prefix="/posts", tags=["posts"])
 app.include_router(bookings_router, prefix="/bookings", tags=["booking"])
+app.include_router(posts_router, prefix="/posts", tags=["posts"])
+
